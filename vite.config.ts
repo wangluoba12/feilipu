82f280dc6cde2363bd2d08cdd96933963df9951b
wangluoba12 <135511561+wangluoba12@users.noreply.github.com>
2026-07-01 16:14:16 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
-      fileName: () => 'index',
+      fileName: () => 'index.mjs',
     },
     rollupOptions: {
       external: [...nodeBuiltins],
